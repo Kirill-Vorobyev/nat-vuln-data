4091c22ba24e009670dc9ed2394a45fc43bd8091
Kirill V <Chicken_Combo@yahoo.com>
2018-02-18 21:46:44 -0500

    Changed jest testPathIgnorePatterns

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import App from './App';
+import Layout from './Components/Layout/Layout';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({adapter: new Adapter()});
+
+describe('<App />', ()=> {
+  it('Should render Layout component', () => {
+      const wrapper = shallow(<App />);
+      expect(wrapper.find(Layout)).toHaveLength(1);
+  });
+})
+
